c4385e66a016a2a4ee9ecd630fa66b3fd545f5aa
sameer <sameerwaseem01@gmail.com>
2020-07-02 15:40:45 +0500

    Updates

---
+++ precache-manifest.c088cbcf784e9fb4808c115670d68fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81edbf92d2440e04a28cd4723dd2c9b0",
+    "url": "/sameerwasim.github.io/index.html"
+  },
+  {
+    "revision": "f62295fa88dcc808e8c7",
+    "url": "/sameerwasim.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc8faf30d0ef5d6c1227",
+    "url": "/sameerwasim.github.io/static/js/2.e80d1da9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sameerwasim.github.io/static/js/2.e80d1da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62295fa88dcc808e8c7",
+    "url": "/sameerwasim.github.io/static/js/main.1eeed0e4.chunk.js"
+  },
+  {
+    "revision": "e4ed86b5619b488230e8",
+    "url": "/sameerwasim.github.io/static/js/runtime-main.ef6f26cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sameerwasim.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
