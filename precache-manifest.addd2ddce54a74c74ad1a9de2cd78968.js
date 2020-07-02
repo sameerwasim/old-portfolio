a3d2eaf85fd4db4e0ef450908206ac52cc991ea7
sameer <sameerwaseem01@gmail.com>
2020-07-02 15:43:31 +0500

    Updates

---
+++ precache-manifest.addd2ddce54a74c74ad1a9de2cd78968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8439fbf3182fd5deae3cf8d14f04fa37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed14206974e94f14761",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc8faf30d0ef5d6c1227",
+    "url": "/static/js/2.e80d1da9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e80d1da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed14206974e94f14761",
+    "url": "/static/js/main.c2fd82e2.chunk.js"
+  },
+  {
+    "revision": "3cb1432ed7456aaac107",
+    "url": "/static/js/runtime-main.5ea268a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
