f2384f4d77bde0e09075441a48153018c79a92f6
sameer <sameerwaseem01@gmail.com>
2020-08-16 01:39:43 +0500

    index page

---
+++ assests/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // Bar Hover Function
+  $(".bar-btn").mouseenter(function(){
+    $(".bar-line").addClass("bar-line-width");
+  });
+  $(".bar-btn").mouseleave(function(){
+    $(".bar-line").removeClass("bar-line-width");
+  });
+
+  $('.name').on("animationend", function(){
+    $('.typewriter-animation').addClass("hide-typewriter-border");
+    $('.skills-text').removeClass("hide");
+    $('.skills-text').addClass("typewriter-animation");
+  });
+
+});
